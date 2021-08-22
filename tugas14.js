22c3d9868d58e75878e10c91c694b90f7d8092e5
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-22 21:42:08 +0700

    Tugas 14 Javascript
    Tuugas ini Kami kumpulkan untuk Niomic

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+var tahunLahir = 1997;
+var pembagi = 37;
+var hasil = tahunLahir/pembagi;
+
+console.log(hasil)
+console.log("ceil : ",Math.ceil(hasil))
+console.log("floor : ", Math.floor(hasil))
+console.log("round : ", Math.round(hasil)) 
